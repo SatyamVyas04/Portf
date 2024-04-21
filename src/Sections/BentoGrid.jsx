05689f572dcbd13766d3aa95e59438a51cf5d7f6
SatyamVyas <vyas.satyam.04@gmail.com>
2024-04-21 16:58:11 +0530

    BentoGridCompleted

--- src/Sections/BentoGrid.jsx
+++ src/Sections/BentoGrid.jsx
@@ -150,7 +150,27 @@ function BentoGrid() {
 						/>
 					</svg>
 				</div>
-				<div className="card col-start-4 row-start-3">Resume Link</div>
+				<a
+					href="SatyamVyasResume.pdf"
+					target="_blank"
+					className="card col-start-4 row-start-3 flex flex-col sm:flex-row gap-4 group cursor-pointer"
+				>
+					<h1>Resume</h1>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6 group-hover:animate-bounce"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+						/>
+					</svg>
+				</a>
 			</div>
 		</div>
 	);
